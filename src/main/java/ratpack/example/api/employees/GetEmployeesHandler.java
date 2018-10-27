e34b4343e8207eba333de205b5b65ef688ae5fa0
gregwhitaker <gwhitake@gmail.com>
2018-10-26 17:23:42 -0700

    Doing work

--- src/main/java/ratpack/example/api/employees/GetEmployeesHandler.java
+++ src/main/java/ratpack/example/api/employees/GetEmployeesHandler.java
@@ -1,9 +1,13 @@
 package ratpack.example.api.employees;
 
 import com.google.inject.Inject;
+import ratpack.example.data.employee.Employee;
 import ratpack.example.service.EmployeeService;
 import ratpack.handling.Context;
 import ratpack.handling.Handler;
+import ratpack.jackson.Jackson;
+
+import java.util.List;
 
 public class GetEmployeesHandler implements Handler {
 
@@ -12,6 +16,24 @@ public class GetEmployeesHandler implements Handler {
 
     @Override
     public void handle(Context ctx) throws Exception {
+        employeeService.getAllEmployees()
+                .then(employees -> {
+                    Response response = new Response();
+                    response.setEmployees(employees);
 
+                    ctx.render(Jackson.json(response));
+                });
+    }
+
+    static class Response {
+        private List<Employee> employees;
+
+        public List<Employee> getEmployees() {
+            return employees;
+        }
+
+        public void setEmployees(List<Employee> employees) {
+            this.employees = employees;
+        }
     }
 }
