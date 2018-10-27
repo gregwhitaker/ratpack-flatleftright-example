35426a307218c2765515c44dc63417bc46badc4f
gregwhitaker <gwhitake@gmail.com>
2018-10-26 17:12:24 -0700

    Doing work

--- src/main/java/ratpack/example/data/DataModule.java
+++ src/main/java/ratpack/example/data/DataModule.java
@@ -1,8 +1,14 @@
 package ratpack.example.data;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import ratpack.example.data.employee.EmployeeDao;
 import ratpack.example.data.widget.WidgetDao;
+
+import javax.sql.DataSource;
 
 public class DataModule extends AbstractModule {
 
@@ -10,5 +16,18 @@ public class DataModule extends AbstractModule {
     protected void configure() {
         bind(EmployeeDao.class);
         bind(WidgetDao.class);
+    }
+
+    @Singleton
+    @Provides
+    public DataSource dataSource() {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDriverClassName("org.h2.jdbcx.JdbcDataSource");
+        hikariConfig.setJdbcUrl("jdbc:h2:./build/database");
+        hikariConfig.setConnectionTestQuery("VALUES 1");
+        hikariConfig.setUsername("sa");
+        hikariConfig.setPassword("sa");
+
+        return new HikariDataSource(hikariConfig);
     }
 }
--- src/main/java/ratpack/example/data/employee/EmployeeDao.java
+++ src/main/java/ratpack/example/data/employee/EmployeeDao.java
@@ -1,8 +1,69 @@
 package ratpack.example.data.employee;
 
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import ratpack.exec.Blocking;
+import ratpack.exec.Promise;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 @Singleton
 public class EmployeeDao {
 
+    private static final String SELECT_EMPLOYEE_SQL = "SELECT * FROM employee WHERE employee_id = ?";
+    private static final String SELECT_EMPLOYEES_SQL = "SELECT * FROM employee";
+
+    @Inject
+    private DataSource dataSource;
+
+    public Promise<Employee> find(Integer id) {
+        return Blocking.get(() -> {
+            try (Connection conn = dataSource.getConnection()) {
+                try (PreparedStatement ps = conn.prepareStatement(SELECT_EMPLOYEE_SQL)) {
+                    ps.setInt(1, id);
+
+                    try (ResultSet rs = ps.executeQuery()) {
+                        if (rs.next()) {
+                            Employee employee = new Employee();
+                            employee.setId(rs.getInt("employee_id"));
+                            employee.setFirstName(rs.getString("employee_firstname"));
+                            employee.setLastName(rs.getString("employee_lastname"));
+
+                            return employee;
+                        }
+                    }
+                }
+            }
+
+            return null;
+        });
+    }
+
+    public Promise<List<Employee>> findAll() {
+        return Blocking.get(() -> {
+            try (Connection conn = dataSource.getConnection()) {
+                try (PreparedStatement ps = conn.prepareStatement(SELECT_EMPLOYEES_SQL)) {
+                    try (ResultSet rs = ps.executeQuery()) {
+                        List<Employee> employees = new ArrayList<>();
+
+                        while(rs.next()) {
+                            Employee employee = new Employee();
+                            employee.setId(rs.getInt("employee_id"));
+                            employee.setFirstName(rs.getString("employee_firstname"));
+                            employee.setLastName(rs.getString("employee_lastname"));
+
+                            employees.add(employee);
+                        }
+
+                        return employees;
+                    }
+                }
+            }
+        });
+    }
 }
