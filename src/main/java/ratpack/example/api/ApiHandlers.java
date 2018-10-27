455aa19314f8a2de00d95c0b3a3ca1bd5aa02e97
gregwhitaker <gwhitake@gmail.com>
2018-10-26 17:26:40 -0700

    Doing work

--- src/main/java/ratpack/example/api/ApiHandlers.java
+++ src/main/java/ratpack/example/api/ApiHandlers.java
@@ -11,9 +11,9 @@ public class ApiHandlers implements Action<Chain> {
     @Override
     public void execute(Chain chain) throws Exception {
 
-        chain.get("/api/v1/employees", GetEmployeesHandler.class);
+        chain.get("api/v1/employees", GetEmployeesHandler.class);
 
-        chain.prefix("/api/v1/reports", c -> {
+        chain.prefix("api/v1/reports", c -> {
            c.get("widgets", GetWidgetsReportHandler.class);
            c.get("employees/:employeeId/widgets", GetEmployeeWidgetsReportHandler.class);
         });
