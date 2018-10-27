cdd79e53ec35508967731cd7e168f9d0255bb459
gregwhitaker <gwhitake@gmail.com>
2018-10-26 17:21:39 -0700

    Doing work

--- src/main/java/ratpack/example/service/EmployeeService.java
+++ src/main/java/ratpack/example/service/EmployeeService.java
@@ -2,7 +2,11 @@ package ratpack.example.service;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import ratpack.example.data.employee.Employee;
 import ratpack.example.data.employee.EmployeeDao;
+import ratpack.exec.Promise;
+
+import java.util.List;
 
 @Singleton
 public class EmployeeService {
@@ -12,5 +16,13 @@ public class EmployeeService {
     @Inject
     public EmployeeService(final EmployeeDao employeeDao) {
         this.employeeDao = employeeDao;
+    }
+
+    public Promise<Employee> getEmployee(Integer id) {
+        return employeeDao.find(id);
+    }
+
+    public Promise<List<Employee>> getAllEmployees() {
+        return employeeDao.findAll();
     }
 }
--- src/main/java/ratpack/example/service/WidgetService.java
+++ src/main/java/ratpack/example/service/WidgetService.java
@@ -1,8 +1,14 @@
 package ratpack.example.service;
 
 import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import ratpack.example.data.widget.Widget;
 import ratpack.example.data.widget.WidgetDao;
+import ratpack.exec.Promise;
 
+import java.util.List;
+
+@Singleton
 public class WidgetService {
 
     private final WidgetDao widgetDao;
@@ -10,5 +16,13 @@ public class WidgetService {
     @Inject
     public WidgetService(final WidgetDao widgetDao) {
         this.widgetDao = widgetDao;
+    }
+
+    public Promise<Widget> getWidget(Integer id) {
+        return widgetDao.find(id);
+    }
+
+    public Promise<List<Widget>> getAllWidgets() {
+        return widgetDao.findAll();
     }
 }
